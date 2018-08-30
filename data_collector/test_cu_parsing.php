97124a383a7653c1e86580f2ee66c4f991e2f8a1
Taeho Lee <developdestroyer@gmail.com>
2018-08-30 18:19:42 +0900

    cu 증정품 db저장기능 추가
    1

--- data_collector/test_cu_parsing.php
+++ data_collector/test_cu_parsing.php
@@ -30,82 +30,155 @@
 
 $PageRequestCount = 1;
 while($isHaveItem==false){
-  $vars = array(
-  "pageIndex"=>$PageRequestCount,
-  "listType"=>"1"
-  //"searchCondition"=>"23"
-);
+    $vars = array(
+    "pageIndex"=>$PageRequestCount,
+    "listType"=>"1"
+    //"searchCondition"=>"23"
+    );
+
+    $snoopy = new Snoopy();
+    $snoopy->httpmethod = "POST";
+    $snoopy->submit("http://cu.bgfretail.com/event/plusAjax.do",$vars);
+
+    //echo $snoopy->results;
+
+
+    //1. 긁어온 문서에서 노드를 찾아내는데 명확성을 부여할 수 있는 HTML부분만 가져옵니다.
+  //$parse_html = splitBetweenStr($snoopy->results,"<div class=\"list_area daily_img\">","</ul>");
+  //2. 그 내용을 기반으로 하나의 노드를 구분짓습니다.
+  $parse_node = splitBetweenStr($snoopy->results,"<div class=\"photo\">","<span class=\"tag\">");
+  //3. 노드들을 순회하며 각 노드의 세부내용을 뽑아냅니다.
+  $cnt = 0;
+  //$sql = "INSERT INTO webtoon(corp, week, updated, toon_name, toon_url, toon_image) VALUES ";
+
+  if(strpos($snoopy->results, "조회된 상품이 없습니다") !== false || $cnt > 5000) {
+      echo "*************************데이터 없음 이제 조회 끝!!";
+      break;
+  } else {
+      echo "데이터 아직 존재함";
+  }
+  $PageRequestCount++;
+
+  echo "<br>-------------------------------------------------------<br>";
+
+  foreach ($parse_node as $value) {
+    $parse_n_plus = 1;
+    if(strpos($value, "1+1") !== false) {
+        $parse_n_plus = 1;
+    }
+    else if(strpos($value, "2+1") !== false) {
+        $parse_n_plus = 2;
+    }
+    else if(strpos($value, "3+1") !== false) {
+        $parse_n_plus = 3;
+    }
+
+    $parse_img_tmp = splitBetweenStr($value,"<img src=\"","\" alt");
+    $parse_name_tmp = splitBetweenStr($value,"<p class=\"prodName\">","</a></p>");
+    $parse_name_tmp2 = explode(">",$parse_name_tmp[0]);
+    //echo "*".$parse_name_tmp2;
+    $parse_price_tmp = splitBetweenStr($value,"<p class=\"prodPrice\"><span>","</span>");
+
+    //$parse_author_merge = "";
+    //foreach ($parse_author_tmp as $value2) {
+    //	$parse_author_merge .= $value2.",";
+    //}
+    //$parse_author_merge=substr($parse_author_merge, 0, -1);
+
+    $parse_img[$cnt] = $parse_img_tmp[0];
+    $parse_name[$cnt] = $parse_name_tmp2[1];
+    $parse_price[$cnt] = $parse_price_tmp[0];
+
+    echo "item".$cnt." : ".$parse_img[$cnt].", ".$parse_name[$cnt].", ".$parse_price[$cnt]."<br>";
+
+
+    $sql = "INSERT INTO cvs_n_plus_one_goods(cvs_name, item_name, category, price, image, detail, upload_date) VALUES('cu', '".$parse_name[$cnt]."', '".$parse_n_plus."',".$parse_price[$cnt].",'".$parse_img[$cnt]."','','".$today."')";
+    $result = mysql_query($sql, $connect);
+
+    //$sql .= "('daum','".$daily[$date]."','".$today."','[".$parse_author_tmp[0]."] ".$parse_title_tmp[0]."','http://webtoon.daum.net/m/webtoon/view/".$parse_url_tmp[0]."','".$parse_image_tmp[0]."'),";
+    $cnt++;
+
+    unset($parse_img_tmp);
+    unset($parse_name_tmp);
+    unset($parse_name_tmp2);
+    unset($parse_price_tmp);
+
+
+  }
+}//while문 끝
+
+//증정품 조사
+if($isHaveItem==false){//
+  $vars = array();
+  $cnt = 0;
+
+  $default_name = array();
+  $default_img = array();
+  $default_price = array();
+  $bonus_name = array();
+  $bonus_img = array();
+  $bonus_price = array();
+
 
   $snoopy = new Snoopy();
   $snoopy->httpmethod = "POST";
-  $snoopy->submit("http://cu.bgfretail.com/event/plusAjax.do",$vars);
+  $snoopy->submit("http://cu.bgfretail.com/event/presentAjax.do",$vars);
 
-  //echo $snoopy->results;
+    //echo $snoopy->results;
 
 
-  //1. 긁어온 문서에서 노드를 찾아내는데 명확성을 부여할 수 있는 HTML부분만 가져옵니다.
-//$parse_html = splitBetweenStr($snoopy->results,"<div class=\"list_area daily_img\">","</ul>");
-//2. 그 내용을 기반으로 하나의 노드를 구분짓습니다.
-$parse_node = splitBetweenStr($snoopy->results,"<div class=\"photo\">","<span class=\"tag\">");
-//3. 노드들을 순회하며 각 노드의 세부내용을 뽑아냅니다.
-$cnt = 0;
-//$sql = "INSERT INTO webtoon(corp, week, updated, toon_name, toon_url, toon_image) VALUES ";
+    //1. 긁어온 문서에서 노드를 찾아내는데 명확성을 부여할 수 있는 HTML부분만 가져옵니다.
+  //$parse_html = splitBetweenStr($snoopy->results,"<div class=\"list_area daily_img\">","</ul>");
+  //2. 그 내용을 기반으로 하나의 노드를 구분짓습니다.
+  $parse_node = splitBetweenStr($snoopy->results,"<div class=\"presentList-w\">","<li>증정행사</li>");
+  $trans_results = str_replace("원</p>", "원</p><!--증정품끝단-->", $snoopy->results);
+  echo "***transe".$trans_results;
+  $parse_node2 = splitBetweenStr($trans_results,"<div class=\"presentList-e\">","<!--증정품끝단-->");
+  //3. 노드들을 순회하며 각 노드의 세부내용을 뽑아냅니다.
 
-if(strpos($snoopy->results, "조회된 상품이 없습니다") !== false) {
-    echo "*************************데이터 없음 이제 조회 끝!!";
-    break;
-} else {
-    echo "데이터 아직 존재함";
-}
-$PageRequestCount++;
 
-echo "<br>-------------------------------------------------------<br>";
+  foreach ($parse_node as $value) {
 
-foreach ($parse_node as $value) {
-  $parse_n_plus = 1;
-  if(strpos($value, "1+1") !== false) {
-      $parse_n_plus = 1;
-  }
-  else if(strpos($value, "2+1") !== false) {
-      $parse_n_plus = 2;
-  }
-  else if(strpos($value, "3+1") !== false) {
-      $parse_n_plus = 3;
+    $parse_img_tmp = splitBetweenStr($value,"<img src=\"","\" alt");
+    $parse_name_tmp = splitBetweenStr($value,"<p class=\"prodName\"><a href=","</a></p>");
+    $parse_name_tmp2 = explode(">",$parse_name_tmp[0]);
+    $parse_price_tmp = splitBetweenStr($value,"<p class=\"prodPrice\"><span>","</span>원");
+
+
+    $default_name[$cnt] = $parse_name_tmp2[1];
+    $default_img[$cnt] = $parse_img_tmp[0];
+    $default_price[$cnt] = str_replace(",", "", $parse_price_tmp[0]);
+    echo "***".$default_name[$cnt].",".$default_img[$cnt].",".$default_price[$cnt]."<br>";
+    $cnt++;
   }
 
-  $parse_img_tmp = splitBetweenStr($value,"<img src=\"","\" alt");
-  $parse_name_tmp = splitBetweenStr($value,"<p class=\"prodName\">","</a></p>");
-  $parse_name_tmp2 = explode(">",$parse_name_tmp[0]);
-  //echo "*".$parse_name_tmp2;
-  $parse_price_tmp = splitBetweenStr($value,"<p class=\"prodPrice\"><span>","</span>");
+  $cnt = 0;
+  foreach ($parse_node2 as $value) {
 
-  //$parse_author_merge = "";
-  //foreach ($parse_author_tmp as $value2) {
-  //	$parse_author_merge .= $value2.",";
-  //}
-  //$parse_author_merge=substr($parse_author_merge, 0, -1);
-
-  $parse_img[$cnt] = $parse_img_tmp[0];
-  $parse_name[$cnt] = $parse_name_tmp2[1];
-  $parse_price[$cnt] = $parse_price_tmp[0];
-
-  echo "item".$cnt." : ".$parse_img[$cnt].", ".$parse_name[$cnt].", ".$parse_price[$cnt]."<br>";
+    $parse_img_tmp = splitBetweenStr($value,"<img src=\"","\" alt");
+    $parse_name_tmp = splitBetweenStr($value,"<p class=\"prodName\"><a href=","</a></p>");
+    $parse_name_tmp2 = explode(">",$parse_name_tmp[0]);
+    $parse_price_tmp = splitBetweenStr($value,"<p class=\"prodPrice\">","원</p>");
 
 
-  $sql = "INSERT INTO cvs_n_plus_one_goods(cvs_name, item_name, category, price, image, detail, upload_date) VALUES('cu', '".$parse_name[$cnt]."', '".$parse_n_plus."',".$parse_price[$cnt].",'".$parse_img[$cnt]."','','".$today."')";
-  $result = mysql_query($sql, $connect);
+    $bonus_name[$cnt] = $parse_name_tmp2[1];
+    $bonus_img[$cnt] = $parse_img_tmp[0];
+    $bonus_price[$cnt] = str_replace(",", "", $parse_price_tmp[0]);
+    echo "***".$bonus_name[$cnt].",".$bonus_img[$cnt].",".$bonus_price[$cnt]."<br>";
+    $cnt++;
+  }
 
-  //$sql .= "('daum','".$daily[$date]."','".$today."','[".$parse_author_tmp[0]."] ".$parse_title_tmp[0]."','http://webtoon.daum.net/m/webtoon/view/".$parse_url_tmp[0]."','".$parse_image_tmp[0]."'),";
-  $cnt++;
+  for($i = 0; $i < $cnt; $i++){
+    $sql = "INSERT INTO cvs_bonus_goods(cvs_name, default_item_name, bonus_item_name, price, bonus_price, default_item_image, bonus_item_image, detail, upload_date) VALUES('cu', '".$default_name[$i]."', '".$bonus_name[$i]."',".$default_price[$i].",".$bonus_price[$i].",'".$default_img[$i]."','".$bonus_img[$i]."','','".$today."')";
+    //echo $sql."<br>";
+    $result = mysql_query($sql, $connect);
+  }
 
-  unset($parse_img_tmp);
-  unset($parse_name_tmp);
-  unset($parse_name_tmp2);
-  unset($parse_price_tmp);
+
+
 
 
 }
-}//while문 끝
 
 if($isHaveItem == false){
   $sql = "INSERT INTO cvs_update_check(cvs_name, upload_date) VALUES('cu','".$today."')";
